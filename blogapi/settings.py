9058d6067b1d3c6e8936bd42fc124ce6c53cef32
S-Delowar <sayed.buet97@gmail.com>
2025-05-04 01:33:08 +0600

    created custom user model

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # initialized apps:
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Extra Configs
+AUTH_USER_MODEL = 'accounts.CustomUser'

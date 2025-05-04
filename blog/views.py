dc2ee996e23f36a3219a3b207da72e4ee616f7ae
S-Delowar <sayed.buet97@gmail.com>
2025-05-04 12:30:36 +0600

    integrated permissions to post apis

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from blog.models import Post
+from blog.permissions import IsAuthorOrReadOnly
+from blog.serializers import PostSerializer
+
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly,]
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)

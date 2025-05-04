8d7d199409f7bc8e9c93ff9f619cd553f1119f70
S-Delowar <sayed.buet97@gmail.com>
2025-05-04 16:00:29 +0600

    unit tests for register api

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+
+from accounts.models import CustomUser
 
 
 # Tests for CustomUser Model
@@ -9,8 +14,7 @@ class CustomUserTests(TestCase):
     def test_create_user(self):
         user = get_user_model().objects.create_user(
             username="testuser", email="testuser@mail.com", password="testpass123"
-        )
-        
+        )  
         self.assertEqual(user.username, "testuser")
         self.assertEqual(user.email, "testuser@mail.com")
         self.assertFalse(user.is_staff)
@@ -22,8 +26,49 @@ class CustomUserTests(TestCase):
         user = get_user_model().objects.create_superuser(
             username="superuser", email="superuser@mail.com", password="super123"
         )
-        
         self.assertEqual(user.username, "superuser")
         self.assertEqual(user.email, "superuser@mail.com")
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+        
+
+# ====================================================
+
+# Tests for "/api/register/" for registering new user
+class UserRegistrationTestCase(APITestCase):
+    def setUp(self):
+        self.register_url = reverse("register")
+       
+        
+    # test for register with all required fields
+    def test_register_user(self):
+        data = {
+            "username": "testuser", "email":"test@mail.com", "password": "test12345"
+        }
+        response = self.client.post(self.register_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(CustomUser.objects.first().email, "test@mail.com")
+
+
+    # test for register without all required fields
+    def test_register_with_missing_fields(self):
+        data = {
+            "username": "user", "email": "", "password": ""
+        }
+        response = self.client.post(self.register_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    
+    # test for register with an email that is already used
+    def test_register_duplicate_email(self):
+        CustomUser.objects.create_user(
+            username="user", email="user@mail.com", password="userpass123"
+        )
+        data = {
+            "username": "new_user",
+            "email": "user@mail.com",
+            "password": "newpass123"
+        }
+        response = self.client.post(self.register_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)

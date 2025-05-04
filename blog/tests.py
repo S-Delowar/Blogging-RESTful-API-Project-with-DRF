ebc9e16ec70661360569138e16665e3914a22a02
S-Delowar <sayed.buet97@gmail.com>
2025-05-04 10:36:24 +0600

    test for Post Model

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from blog.models import Post
+
+
+# Tests for Post Model
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@mail.com",
+            password="testpass"
+        )
+        
+        cls.post = Post.objects.create(
+            author = cls.user,
+            title = "A Good Title",
+            content = "Nice Body Content"
+        )
+        
+    # post creation test
+    def test_post_creation(self):
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(self.post.title, "A Good Title")
+        self.assertEqual(self.post.content, "Nice Body Content")
+        self.assertEqual(str(self.post), "A Good Title")
